89ee36fb9b2d4c6781c5843cc88ff472b1a989c6
Pierre Lundström <pierre.lundstrom@learnet.se>
2019-09-06 10:39:42 +0200

    first commit

---
+++ Uppgift11.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import javax.swing.*;
+
+public class Uppgift11 { 
+   public static void main(String[] args) {
+	   /*/ Calender.getInstance().getTime().toString(); /*/
+	   JOptionPane.showMessageDialog(null, "Datum och tid:" + "\n" + Calendar.getInstance().getTime().toString());
+
+   }
+}
---
+++ Uppgift12.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import javax.swing.*;
+
+public class Uppgift12 { 
+   public static void main(String[] args) {
+	   /*/ Calender.getInstance().getTime().toString(); /*/
+	   
+	   JOptionPane.showMessageDialog(null, "Are you here?", JOptionPane.QUESTION_MESSAGE);
+       
+   }
+}
